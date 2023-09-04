feadadb7c4a9d28ccdd304c368aa7f87ae2311b4
manonmission88 <manishnewray88@gmail.com>
2023-09-04 17:31:16 -0400

    largestvalue in each level of binary tree

---
+++ binaryTree/largest_value_level.py
@@ -0,0 +1,44 @@
+'''
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+
+
+
+'''
+from collections import deque
+import math
+from tree_node import *
+def largest_value(root):
+    if not root:
+        return None 
+    q=deque()
+    q.append(root)
+    q.append(None)
+    max_value = -math.inf 
+    result = []
+    while q:
+        node = q.popleft()
+        if node:
+            max_value = max(max_value,node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        else:
+            result.append(max_value)
+            max_value=-math.inf
+            if q:
+                q.append(None)
+    return result 
+root=root_from_list([1,3,2,5,3,None,9])
+print(largest_value(root))#[1,3,9]   
